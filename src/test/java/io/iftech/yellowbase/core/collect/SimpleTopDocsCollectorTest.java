e5d3e6ac6db0ff88ee0f2946b6733360a2b0a037
onesuper <onesuperclark@gmail.com>
2019-08-04 01:44:43 +0800

    Fix collector uts

--- src/test/java/io/iftech/yellowbase/core/collect/SimpleTopDocsCollectorTest.java
+++ src/test/java/io/iftech/yellowbase/core/collect/SimpleTopDocsCollectorTest.java
@@ -9,19 +9,19 @@ public class SimpleTopDocsCollectorTest {
     @Test
     public void testSimpleTopDocsCollector() {
 
-        TopDocsCollector collector = new SimpleTopDocsCollector();
+        TopDocsCollector<String> collector = new SimpleTopDocsCollector<String>();
 
-        collector.collect("1");
-        collector.collect("2");
-        collector.collect("3");
+        collector.collect("1", 1);
+        collector.collect("2", 2);
+        collector.collect("3", 3);
 
         TopDocs topDocs = collector.topDocs();
 
         Truth.assertThat(topDocs.totalHits).isEqualTo(3);
         Truth.assertThat(topDocs.scoreDocs).hasLength(3);
         Truth.assertThat(topDocs.scoreDocs).asList().containsExactly(
-            new ScoreDoc("1", 0),
-            new ScoreDoc("2", 0),
-            new ScoreDoc("3", 0));
+            new ScoreDoc<>("1", 1),
+            new ScoreDoc<>("2", 2),
+            new ScoreDoc<>("3", 3));
     }
 }
