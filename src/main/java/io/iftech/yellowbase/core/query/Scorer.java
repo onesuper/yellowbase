58c1f2637158ca5f14618f08dbdf2ed56b68ee10
onesuper <onesuperclark@gmail.com>
2019-08-04 02:14:29 +0800

    Add foreach method to Scorer

--- src/main/java/io/iftech/yellowbase/core/query/Scorer.java
+++ src/main/java/io/iftech/yellowbase/core/query/Scorer.java
@@ -1,10 +1,18 @@
 package io.iftech.yellowbase.core.query;
 
 import io.iftech.yellowbase.core.docset.DocSet;
+import java.util.function.BiConsumer;
 
 public interface Scorer {
 
     float score();
 
     DocSet<Integer> docSet();
+
+    default void foreach(BiConsumer<Integer, Float> callback) {
+        DocSet<Integer> ds = docSet();
+        while (ds.next() != DocSet.NO_MORE_DOCS) {
+            callback.accept(ds.docId(), score());
+        }
+    }
 }
