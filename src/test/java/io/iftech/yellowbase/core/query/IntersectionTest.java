625a01319553ab3bdd44376023b947418688d572
onesuper <onesuperclark@gmail.com>
2019-08-09 22:52:54 +0800

    Test functionality for Intersection

--- src/test/java/io/iftech/yellowbase/core/query/IntersectionTest.java
+++ src/test/java/io/iftech/yellowbase/core/query/IntersectionTest.java
@@ -55,7 +55,7 @@ public class IntersectionTest {
     public void noIntersection() {
         DocSet<Integer> left = new ListDocSet<>(ImmutableList.of(1, 3, 5, 7, 9));
         DocSet<Integer> right = new ListDocSet<>(ImmutableList.of(2, 4, 6, 8, 10));
-        DocSet<Integer> intersection =Intersection.of(left, right);
+        DocSet<Integer> intersection = Intersection.of(left, right);
         Truth.assertThat(intersection.next()).isFalse();
         Truth.assertThat(intersection.docId()).isNull();
     }
@@ -64,7 +64,7 @@ public class IntersectionTest {
     public void emptyIntersectBeing() {
         DocSet<Integer> left = new ListDocSet<>(ImmutableList.<Integer>of());
         DocSet<Integer> right = new ListDocSet<>(ImmutableList.of(2, 4, 6));
-        DocSet<Integer> intersection =Intersection.of(left, right);
+        DocSet<Integer> intersection = Intersection.of(left, right);
         Truth.assertThat(intersection.next()).isFalse();
     }
 
@@ -72,7 +72,24 @@ public class IntersectionTest {
     public void beingIntersectEmpty() {
         DocSet<Integer> left = new ListDocSet<>(ImmutableList.of(2, 4, 6));
         DocSet<Integer> right = new ListDocSet<>(ImmutableList.<Integer>of());
-        DocSet<Integer> intersection =Intersection.of(left, right);
+        DocSet<Integer> intersection = Intersection.of(left, right);
         Truth.assertThat(intersection.next()).isFalse();
+    }
+
+    @Test
+    public void operationIsNotFunctional() {
+
+        DocSet<Integer> left = new ListDocSet<>(ImmutableList.of(1, 2, 3, 4, 5, 6));
+        DocSet<Integer> right = new ListDocSet<>(ImmutableList.of(3, 6));
+        DocSet<Integer> intersection1 = Intersection.of(left, right);
+        Truth.assertThat(intersection1.next()).isTrue();
+
+        Truth.assertThat(intersection1.docId()).isEqualTo(3);
+        Truth.assertThat(intersection1.next()).isTrue();
+        Truth.assertThat(intersection1.docId()).isEqualTo(6);
+        Truth.assertThat(intersection1.next()).isFalse();
+
+        DocSet<Integer> intersection2 = Intersection.of(left, right);
+        Truth.assertThat(intersection2.next()).isFalse();
     }
 }
