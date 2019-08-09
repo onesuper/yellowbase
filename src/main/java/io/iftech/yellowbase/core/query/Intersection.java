c96fc4b283033da9d1d0f9fa3f13172771f0fce7
onesuper <onesuperclark@gmail.com>
2019-08-09 17:51:50 +0800

    Make Intersection implementation of DocSet

--- src/main/java/io/iftech/yellowbase/core/query/Intersection.java
+++ src/main/java/io/iftech/yellowbase/core/query/Intersection.java
@@ -6,7 +6,7 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Intersection<DocId extends Comparable<DocId>> {
+public class Intersection<DocId extends Comparable<DocId>> implements DocSet<DocId> {
 
     private final DocSet<DocId> left;
     private final DocSet<DocId> right;
@@ -31,60 +31,55 @@ public class Intersection<DocId extends Comparable<DocId>> {
             return new Intersection<>(head, queue.poll());
         }
 
-        return new Intersection<>(head, of(queue).docSet());
+        return new Intersection<>(head, of(queue));
     }
 
-    public DocSet<DocId> docSet() {
-        return new DocSet<DocId>() {
+    /**
+     * left 和 right 文档集合通过在互相在对方中查询自己当前的文档来实现集合求交的过程
+     *
+     * 使用 advance 方法可以提高在集合中查询指定文档的效率
+     *
+     * @return 当 left 或 right 中任何一个为空时，返回 false
+     */
+    @Override
+    public boolean next() {
 
-            /**
-             * left 和 right 文档集合通过在互相在对方中查询自己当前的文档来实现集合求交的过程
-             *
-             * 使用 advance 方法可以提高在集合中查询指定文档的效率
-             *
-             * @return 当 left 或 right 中任何一个为空时，返回 false
-             */
-            @Override
-            public boolean next() {
+        if (!left.next()) {
+            return false;
+        }
 
-                if (!left.next()) {
-                    return false;
-                }
+        while (true) {
 
-                while (true) {
+            // 在 right 中找 left 的当前元素
+            DocId target = left.docId();
 
-                    // 在 right 中找 left 的当前元素
-                    DocId target = left.docId();
-
-                    if (!right.advance(target)) {
-                        return false;
-                    }
-
-                    if (right.docId().equals(target)) {
-                        return true;
-                    }
-
-                    // 在 left 中找 right 的当前元素
-                    target = right.docId();
-                    if (!left.advance(target)) {
-                        return false;
-                    }
-
-                    if (left.docId().equals(target)) {
-                        return true;
-                    }
-                }
+            if (!right.advance(target)) {
+                return false;
             }
 
-            @Override
-            public DocId docId() {
-                return left.docId();
+            if (right.docId().equals(target)) {
+                return true;
             }
 
-            @Override
-            public long size() {
-                return left.size();
+            // 在 left 中找 right 的当前元素
+            target = right.docId();
+            if (!left.advance(target)) {
+                return false;
             }
-        };
+
+            if (left.docId().equals(target)) {
+                return true;
+            }
+        }
+    }
+
+    @Override
+    public DocId docId() {
+        return left.docId();
+    }
+
+    @Override
+    public long size() {
+        return left.size();
     }
 }
