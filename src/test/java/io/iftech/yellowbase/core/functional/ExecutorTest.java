b315e07f7fe326aa71e356a694a1abb8affd4b77
onesuper <onesuperclark@gmail.com>
2019-08-11 00:16:39 +0800

    Fix executor test

--- src/test/java/io/iftech/yellowbase/core/functional/ExecutorTest.java
+++ src/test/java/io/iftech/yellowbase/core/functional/ExecutorTest.java
@@ -1,9 +1,9 @@
-package io.iftech.yellowbase.core;
+package io.iftech.yellowbase.core.functional;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.truth.Truth;
-import io.iftech.yellowbase.core.functional.Executor;
 import java.util.List;
+import java.util.concurrent.Executors;
 import org.junit.Test;
 
 public class ExecutorTest {
@@ -11,7 +11,7 @@ public class ExecutorTest {
     @Test
     public void executorCanMapValues() {
 
-        Executor executor = new Executor();
+        Executor executor = new Executor(Executors.newSingleThreadExecutor());
         List<String> result = executor.map((i) -> i + "", ImmutableList.of(1, 2, 3));
         Truth.assertThat(result).containsExactly("1", "2", "3");
     }
