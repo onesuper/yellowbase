6404256326e543c767ea025ca4049e2d961ab0f0
onesuper <onesuperclark@gmail.com>
2019-08-23 18:03:14 +0800

    Pass weight to boolean query

--- src/main/java/io/iftech/yellowbase/core/query/BooleanQuery.java
+++ src/main/java/io/iftech/yellowbase/core/query/BooleanQuery.java
@@ -3,17 +3,13 @@ package io.iftech.yellowbase.core.query;
 import io.iftech.yellowbase.core.SegmentReader;
 import io.iftech.yellowbase.core.docset.DocSet;
 import io.iftech.yellowbase.core.query.BooleanClause.Kind;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
-/**
- * BooleanQuery 由多个 BooleanClause 节点组成
- *
- * 每个 BooleanClause 节点可以是任意的 Query（例如 TermQuery）
- *
- * 或是一个 BooleanQuery
- */
 public class BooleanQuery extends Query {
 
     final Map<Kind, Collection<Query>> clauseByKind;
@@ -36,14 +32,26 @@ public class BooleanQuery extends Query {
 
     @Override
     public Weight weight() {
-        return new BooleanWeight(clauseByKind);
+        Map<Kind, Collection<Weight>> weightsByKinds = new HashMap<>();
+
+        clauseByKind.forEach((kind, subClauses) -> {
+
+            Consumer<Query> weighter = clause ->
+                weightsByKinds.computeIfAbsent(kind, k -> new ArrayList<>())
+                    .add(clause.weight());
+
+            subClauses.forEach(weighter);
+        });
+
+        return new BooleanWeight(weightsByKinds);
     }
 
     private class BooleanWeight implements Weight {
-        private Map<Kind, Collection<Query>> clauseByKind;
 
-        public BooleanWeight(Map<Kind, Collection<Query>> clauseByKind) {
-            this.clauseByKind = clauseByKind;
+        private Map<Kind, Collection<Weight>> subWeightsByKind;
+
+        public BooleanWeight(Map<Kind, Collection<Weight>> subWeightsByKind) {
+            this.subWeightsByKind = subWeightsByKind;
         }
 
         @Override
